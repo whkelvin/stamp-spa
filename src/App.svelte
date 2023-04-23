9671a75ba9f801eb5ef326f25cd9f63c0d23fd0c
whkelvin <whkelvin.hsu@gmail.com>
2023-04-23 14:35:03 -0300

    set up daisyui

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
-  import "./app.css";
 </script>
 
-<div class="text-white bg-black">hello</div>
+<button class="btn">Hello daisyui</button>
