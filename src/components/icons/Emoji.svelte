25c795d0aa53975fb7a4e8546344498340168c8f
whkelvin <whkelvin.hsu@gmail.com>
2023-05-05 17:17:01 -0300

    added in emoji component

---
+++ src/components/icons/Emoji.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let label;
+  export let symbol;
+</script>
+
+<span
+  role="img"
+  aria-label={label ? label : ""}
+  aria-hidden={label ? "false" : "true"}
+>
+  {symbol}
+</span>
+
+<style>
+</style>
