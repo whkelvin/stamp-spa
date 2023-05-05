73a5f456d5a977f0fcafb0027fca20a2dd299304
whkelvin <whkelvin.hsu@gmail.com>
2023-05-05 17:18:18 -0300

    refactoring and set up coming soon modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
+  import NavBar from "./components/NavBar.svelte";
   import Router from "svelte-spa-router";
   import routes from "./routes";
+  import ComingSoonModal from "./components/ComingSoonModal.svelte";
 </script>
 
 <main class="w-full">
-  <div class="navbar bg-neutral text-neutral-content w-full">
-    <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block h-5 w-5 stroke-current"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          /></svg
-        >
-      </button>
-    </div>
-    <div class="flex-1">
-      <div class="ml-4 text-xl font-bold text-primary">Stamp</div>
-    </div>
-  </div>
+  <NavBar />
   <Router {routes} />
+  <ComingSoonModal />
 </main>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import StampIcon from "./icons/StampIcon.svelte";
+</script>
+
+<div class="navbar bg-neutral text-neutral-content w-full">
+  <div class="flex-none invisible">
+    <button class="btn btn-square btn-ghost">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="inline-block h-5 w-5 stroke-current"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        /></svg
+      >
+    </button>
+  </div>
+  <div class="flex-1">
+    <StampIcon />
+    <div class="ml-4 text-xl font-black text-primary">Stamp</div>
+  </div>
+</div>
